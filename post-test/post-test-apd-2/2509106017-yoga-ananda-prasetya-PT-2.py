3cc3e1686324c1730647e4ddffc14f572a566b6c
Yoga Ananda Prasetya <yogaananda589@gmail.com>
2025-09-21 14:40:56 +0800

    Mengganti tipe data berat dan tinggi badan

--- post-test/post-test-apd-2/2509106017-yoga-ananda-prasetya-PT-2.py
+++ post-test/post-test-apd-2/2509106017-yoga-ananda-prasetya-PT-2.py
@@ -5,8 +5,8 @@ print("-" * 67)
 
 # Bagian Input nama, tinggi dan berat badan
 nama = str(input(f"{'Nama pasien' :32}: "))
-tinggi_badan = int(input(f"{'Tinggi badan pasien (dalam cm)' :32}: "))
-berat_badan = int(input(f"{'Berat badan pasien (dalam kg)' :32}: "))
+tinggi_badan = float(input(f"{'Tinggi badan pasien (dalam cm)' :32}: "))
+berat_badan = float(input(f"{'Berat badan pasien (dalam kg)' :32}: "))
 
 # Bagian menghitung berat badan serta pengecekan berat badan
 berat_ideal = tinggi_badan - 100
@@ -20,8 +20,8 @@ print("-" * 67)
 
 # Bagian isi dari tabel akhir
 print(f"{'|'} {'Nama Pasien' :30}: {nama} {' ' * (30 - len(nama))} {'|'}")
-print(f"{'|'} {'Tinggi Badan' :30}: {tinggi_badan:.2f} {' ' * (27 - len(str(tinggi_badan)))} {'|'}")
-print(f"{'|'} {"Berat Badan" :30}: {berat_badan:.2f} {' ' * (27 - len(str(berat_badan)))} {'|'}")
-print(f"{'|'} {'Berat Ideal' :30}: {berat_ideal:.2f} {' ' * (27 - len(str(berat_ideal)))} {'|'}")
+print(f"{'|'} {'Tinggi Badan' :30}: {tinggi_badan:.2f} {' ' * (29 - len(str(tinggi_badan)))} {'|'}")
+print(f"{'|'} {"Berat Badan" :30}: {berat_badan:.2f} {' ' * (29 - len(str(berat_badan)))} {'|'}")
+print(f"{'|'} {'Berat Ideal' :30}: {berat_ideal:.2f} {' ' * (29 - len(str(berat_ideal)))} {'|'}")
 print(f"{'|'} {'Status' :30}: {status[int(isKelebihan)]} {' ' * (30 - len(str(status[int(isKelebihan)])))} {'|'}")
-print("-" * 67)
+print("-" * 67) 
